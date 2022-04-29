67b3ca9e808a1ac3ab8993e95bbbd32d615ddd11
Alexandre-st <alexandre.saintprix@gmail.com>
2022-04-29 22:01:15 +0200

    Add hero part

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import Cellular from '../../assets/home/desktop/image-hero-phone.png';
+import CellularPhone from '../../assets/home/desktop/image-hero-phone.png';
+import CellularTablet from '../../assets/home/desktop/image-hero-tablet.png';
+import CellularDesktop from '../../assets/home/desktop/image-hero-desktop.png';
 
 const Home = () => {
   return ( 
@@ -8,10 +10,16 @@ const Home = () => {
       <div className="hero-content">
         <div className="hero-content-text">
           <h1>Award-winning custom designs and digital branding solutions</h1>
-          <p>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
+          <p className="text">With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
           <button className="button">Learn More</button>
         </div>
-        <img className="hero-content-img" src={Cellular} alt="Cellular Frame" />
+        <div className="hero-content-img">
+          <picture>
+            <source media="(min-width: 1064px)" srcSet={CellularDesktop} />
+            <source media="(min-width: 768px)" srcSet={CellularTablet} />
+            <img src={CellularPhone} alt="Cellular Frame" />
+          </picture>
+        </div>
       </div>
     </section>
   );
